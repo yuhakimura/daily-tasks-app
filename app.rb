5927dcda675bf380f588c0f444483edad8478929
yuhakimura <yuhakimura@gmail.com>
2022-09-27 17:14:23 +0000

    ログイン機能実装

--- app.rb
+++ app.rb
@@ -3,6 +3,38 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models.rb'
 
+enable :sessions
+
 get '/' do
   erb :index
+end
+
+get '/signin' do
+    erb :sign_in
+end
+
+get '/signup' do
+    erb :sign_up
+end
+
+post '/signin' do
+    user = User.find_by(mail: params[:mail])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect '/'
+end
+
+post '/signup' do
+    user = User.create(mail: params[:mail], name: params[:name], password: params[:password],
+                        password_confirmation: params[:password_confirmation])
+    if user.persisted?
+        session[:user] = @user.id
+    end
+    redirect '/'
+end
+
+get '/signout' do
+    session[:user] = nil
+    redirect '/'
 end
--- models.rb
+++ models.rb
@@ -2,3 +2,13 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+    has_secure_password
+    validates :mail,
+        presence: true,
+        format: {with:/\A.+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+\z/}
+    validates :password,
+        format: {with:/(?=.*?[a-z])(?=.*?[0-9])/},
+        length: {in: 5..10}
+end
